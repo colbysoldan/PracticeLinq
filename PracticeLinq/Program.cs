4fa7cb2006b742b105c03c5cdd315c50d908b45b
Colby Soldan <colbylsoldan@gmail.com>
2022-06-20 13:51:27 -0500

    finished assignment

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGames = new List<string>
+        {
+            "Borderlands",
+            "Assassin's Creed",
+            "Spyro",
+            "Halo",
+            "Call of Duty",
+            "Kingdom Hearts",
+            "World of Warcraft",
+            "The Elder Scrolls",
+            "Donkey Kong"
+        };
+
+            var orderedList = videoGames.OrderBy(x => x.Length).ToList();
+
+            for (int i=0; i < orderedList.Count; i++)
+            {
+                Console.WriteLine($"{orderedList[i]}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
